15ff3f078d5fc65206e51f2820ab0ca3de5c372e
Ian Keen <iankeen82@gmail.com>
2016-09-16 15:10:48 -0700

    missing protocol conformance

--- Sources/Common/JSONSerialization+Extensions.swift
+++ Sources/Common/JSONSerialization+Extensions.swift
@@ -11,3 +11,33 @@ public extension JSONSerialization {
         return Data(bytes: bytes)
     }
 }
+
+extension JSON: DictionaryRepresentable {
+    public func makeDictionary() -> [String : Any] {
+        guard case .object(let object) = self else { return [:] }
+        
+        var result = [String: Any]()
+        for (key, value) in object {
+            guard let value = value.dictionaryValue else { continue }
+            result[key] = value
+        }
+        return result
+    }
+}
+extension JSON: DictionaryValueRepresentable {
+    public var dictionaryValue: Any? {
+        switch self {
+        case .array(let array): return array.flatMap { $0.dictionaryValue }
+        case .object: return self.makeDictionary()
+        case .null: return nil
+        case .number(let number):
+            switch number {
+            case .double(let value): return value
+            case .integer(let value): return value
+            case .unsignedInteger(let value): return Int(value)
+            }
+        case .boolean(let value): return value
+        case .string(let value): return value
+        }
+    }
+}
